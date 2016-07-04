c4dab3b8ed20c3e043e6bc30bef44fe69e55bbec
Alan George <ageorge@alan-george.com>
2016-07-04 16:09:51 -0700

    sync

--- src/main/scala/interviewquestions/BinarySearchTree.java
+++ src/main/scala/interviewquestions/BinarySearchTree.java
@@ -1,14 +1,23 @@
 package interviewquestions;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Stack;
 import java.util.function.Function;
 
 public class BinarySearchTree {
     private Node root = null;
 
+    @SuppressWarnings("WeakerAccess")
+    public BinarySearchTree() {
+    }
+
+    public BinarySearchTree(Node root) {
+        this.root = root;
+    }
+
+    @SuppressWarnings("WeakerAccess")
     public void put(int value) {
         if (root == null) {
             root = new Node(value);
@@ -36,6 +45,7 @@ public class BinarySearchTree {
         }
     }
 
+    @SuppressWarnings("WeakerAccess")
     public List<Object> foreach(Function<Integer, Object> f) {
         List<Object> results = new ArrayList<>();
         Stack<Node> stack = new Stack<>();
@@ -61,8 +71,66 @@ public class BinarySearchTree {
         return results;
     }
 
-    public class Node {
-        public Node(int value) {
+    @SuppressWarnings("WeakerAccess")
+    public int max() {
+        if (root == null) throw new IllegalStateException("root is null");
+
+        Node node = root;
+        int max;
+
+        while (true) {
+            max = node.value;
+            if (node.right != null) {
+                node = node.right;
+            } else {
+                break;
+            }
+        }
+        return max;
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public int secodHighestMax() {
+        if (root == null) throw new IllegalStateException("root is null");
+
+        Node node = root;
+        int max;
+
+        while (true) {
+            max = node.value;
+            if (node.right != null) {
+                node = node.right;
+            } else {
+                if (node.left != null) {
+                    BinarySearchTree leftTree = new BinarySearchTree(node.left);
+                    max = leftTree.max();
+                }
+                break;
+            }
+        }
+        return max;
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public int min() {
+        if (root == null) throw new IllegalStateException("root is null");
+
+        Node node = root;
+        int min;
+
+        while (true) {
+            min = node.value;
+            if (node.left != null) {
+                node = node.left;
+            } else {
+                break;
+            }
+        }
+        return min;
+    }
+
+    private class Node {
+        Node(int value) {
             this.value = value;
         }
 
@@ -71,17 +139,22 @@ public class BinarySearchTree {
     }
 
     public static void main(String[] args) {
+        final int numberOfNodes = 30;
         BinarySearchTree binarySearchTree = new BinarySearchTree();
 
-        binarySearchTree.put(80);
-        binarySearchTree.put(20);
-        binarySearchTree.put(30);
-        binarySearchTree.put(60);
-        binarySearchTree.put(10);
+        SecureRandom random = new SecureRandom();
+
+        for (int i = 0; i < numberOfNodes; i++) {
+            binarySearchTree.put(random.nextInt());
+        }
 
         binarySearchTree.foreach(i -> {
-            System.out.println("foreach: " + i);
+            System.out.printf("value: %d\n", i);
             return null;
         });
+
+        System.out.println("max: " + binarySearchTree.max());
+        System.out.println("min: " + binarySearchTree.min());
+        System.out.println("secondHighestMax: " + binarySearchTree.secodHighestMax());
     }
 }
